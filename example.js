36b47025ffb1e913c03cefcafe8d66368cb3c26e
Omar Richardson <omar.online@mailbox.org>
2019-11-02 12:28:22 +0100

    Splitted example and sketch

---
+++ example.js
@@ -0,0 +1,54 @@
+
+// A simple Particle class
+class Particle {
+  constructor(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.slice();
+    this.lifespan = 255;
+  }
+  run() {
+    this.update();
+    this.display();
+  }
+  // Method to update position
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+  }
+  // Method to display
+  display() {
+    stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(127, this.lifespan);
+    ellipse(this.position.x, this.position.y, 12, 12);
+  }
+  // Is the particle still useful?
+  isDead() {
+    return this.lifespan < 0;
+  }
+}
+
+
+
+
+
+let ParticleSystem = function (position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function () {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function () {
+  for (let i = this.particles.length - 1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
