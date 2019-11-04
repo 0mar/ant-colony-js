65387c160410e8af353ff8d20c3a20a217dc7151
Omar Richardson <omar.online@mailbox.org>
2019-11-04 20:02:24 +0100

    Simulation works

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let scene;
-
+let size_x = 800;
+let size_y = 800;
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(size_x, size_y);
   scene = new Scene();
 }
 
@@ -18,14 +19,14 @@ function distance(pos1, pos2) {
   return Math.sqrt(del_x * del_x + del_y * del_y);
 }
 
-class Parameters {
+class Params {
   constructor() {
-    this.num_ants = 1;
-    this.num_nodes = 5;
+    this.num_ants = 20;
+    this.num_vertexs = 50;
     this.ant_size = 12;
     this.ant_speed = 10;
-    this.pheromone_decay = 0.005;
-    this.pheromone_deposit = 100;
+    this.pheromone_decay = 0.0005;
+    this.pheromone_deposit = 10;
     this.time_delay = 10;
     this.dt = 0.2;
     this.arbitraryness = 0.0;
@@ -36,10 +37,10 @@ class Scene {
   constructor() {
     this.time = 0;
     this.counter = 0;
-    this.params = new Parameters();
-    this.nest_node = null;
-    this.food_nodes = [];
-    this.graph = this.test_graph();
+    this.params = new Params();
+    this.nest_vertex = null;
+    this.food_vertexs = [];
+    this.graph = this.geometric_graph();
     this.ants = [];
     this.create_colony();
   }
@@ -64,20 +65,45 @@ class Scene {
     let num_els = 4;
     let positions = [[width / 2, 10], [10, height / 2], [width - 10, height / 2], [width / 2, height - 10]]
     let graph = new Graph();
-    let node_list = []; // I don't like this.
+    let vertex_list = []; // I don't like this. Make graphs have vertex list
     for (let i = 0; i < num_els; i++) {
-      let node = new Node(positions[i]);
-      node_list.push(node);
-      graph.add_node(node);
+      let vertex = new Vertex(positions[i]);
+      vertex_list.push(vertex);
+      graph.add_vertex(vertex);
       for (let j = 0; j < i; j++) {
-        graph.add_edge(node, node_list[j]);
+        graph.add_edge(vertex, vertex_list[j]);
       }
     }
     graph.remove_edge(Array.from(graph.edges)[0]);
-    this.nest_node = Array.from(graph.nodes)[0];
-    this.food_nodes = [Array.from(graph.nodes)[1]];
-    Array.from(graph.nodes)[1].food = 1; // todo: make nice
-    this.nest_node.is_nest=true;
+    this.nest_vertex = Array.from(graph.vertexs)[0];
+    this.food_vertexs = [Array.from(graph.vertexs)[1]];
+    Array.from(graph.vertexs)[1].food = 1; // todo: make nice
+    this.nest_vertex.is_nest = true;
+    return graph;
+  }
+
+  geometric_graph() {
+    let graph = new Graph();
+    do {
+      graph.clear();
+      let vertex_list = [];
+      let max_dist = 125;
+      for (let i = 0; i < this.params.num_vertexs; i++) {
+        let vertex = new Vertex([random() * size_x, random() * size_y]);
+        graph.add_vertex(vertex);
+        vertex_list.push(vertex);
+        for (let j = 0; j < i; j++) {
+          if (distance(vertex.position, vertex_list[j].position) < max_dist) {
+            graph.add_edge(vertex, vertex_list[j]);
+            console.log("Edge added on distance ",distance(vertex.position, vertex_list[j].position))
+          }
+        }
+      }
+      this.nest_vertex = Array.from(graph.vertexs)[0];
+      this.food_vertexs = [Array.from(graph.vertexs)[1]];
+      Array.from(graph.vertexs)[1].food = 1; // todo: make nice
+      this.nest_vertex.is_nest = true;
+    } while (!graph.has_path(this.nest_vertex, this.food_vertexs[0]));
     return graph;
   }
 
@@ -90,7 +116,7 @@ class Scene {
   }
 }
 
-class Node {
+class Vertex {
   constructor(position) {
     this.position = position;
     this.edges = new Set();
@@ -102,9 +128,9 @@ class Node {
     return this.edges.has(edge);
   }
 
-  find_edge(other_node) {
+  find_edge(other_vertex) {
     for (let edge of this.edges) {
-      if (edge.other_node(this) === other_node) {
+      if (edge.other_vertex(this) === other_vertex) {
         return edge;
       }
     }
@@ -119,94 +145,124 @@ class Node {
       fill(100, 10, 10);
     }
     if (this.is_nest) {
-      fill(30,150,50);
+      fill(30, 150, 50);
     }
     ellipse(this.position[0], this.position[1], 50, 50);
   }
 }
 
 class Edge {
-  constructor(node1, node2) {
-    this.node1 = node1;
-    this.node2 = node2;
-    node1.edges.add(this);
-    node2.edges.add(this);
-    this.weight = distance(node1.position, node2.position);
+  constructor(vertex1, vertex2) {
+    this.vertex1 = vertex1;
+    this.vertex2 = vertex2;
+    vertex1.edges.add(this);
+    vertex2.edges.add(this);
+    this.weight = distance(vertex1.position, vertex2.position);
     this.pheromone = 0.1;
-    this.del_x = node2.position[0] - node1.position[0];
-    this.del_y = node2.position[1] - node1.position[1];
+    this.del_x = vertex2.position[0] - vertex1.position[0];
+    this.del_y = vertex2.position[1] - vertex1.position[1];
   }
 
-  other_node(node) {
-    if (this.node1 === node) {
-      return this.node2;
-    } else if (this.node2 === node) {
-      return this.node1;
+  other_vertex(vertex) {
+    if (this.vertex1 === vertex) {
+      return this.vertex2;
+    } else if (this.vertex2 === vertex) {
+      return this.vertex1;
     } else {
-      console.log("Node is not connected to edge");
+      console.log("Vertex is not connected to edge");
       return null;
     }
   }
 
-  has_node(node) {
-    return this.node1===node || this.node2 === node;
+  has_vertex(vertex) {
+    return this.vertex1 === vertex || this.vertex2 === vertex;
   }
 
-  get_position(start_node, progress) {
+  get_position(start_vertex, progress) {
     let ratio = progress;
-    if (this.node2 === start_node) {
+    if (this.vertex2 === start_vertex) {
       ratio = - progress;
-    } else if (this.node1 !== start_node) {
-      console.log("Start node is not connected to edge");
+    } else if (this.vertex1 !== start_vertex) {
+      console.log("Start vertex is not connected to edge");
     }
-    return [start_node.position[0] + ratio * this.del_x, start_node.position[1] + ratio * this.del_y];
+    return [start_vertex.position[0] + ratio * this.del_x, start_vertex.position[1] + ratio * this.del_y];
   }
 
   display() {
-    stroke(127*(2 - this.pheromone));
-    line(this.node1.position[0], this.node1.position[1], this.node2.position[0], this.node2.position[1]);
+    stroke(127 * (2 - this.pheromone));
+    line(this.vertex1.position[0], this.vertex1.position[1], this.vertex2.position[0], this.vertex2.position[1]);
   }
 }
 
 class Graph {
   constructor() {
-    this.nodes = new Set();
+    this.vertexs = new Set();
     this.edges = new Set();
+    this.valid = false;
   }
 
-  add_node(node) {
-    this.nodes.add(node);
+  add_vertex(vertex) {
+    this.vertexs.add(vertex);
   }
 
-  remove_node(node) {
-    this.nodes.delete(node);
+  remove_vertex(vertex) {
+    this.vertexs.delete(vertex);
   }
 
-  add_edge(node1, node2) {
+  add_edge(vertex1, vertex2) {
     let edge_exists_already = false;
-    for (let edge of node1.edges) {
-      edge_exists_already |= edge.other_node(node1) == node2;
+    for (let edge of vertex1.edges) {
+      edge_exists_already = edge_exists_already || edge.other_vertex(vertex1) == vertex2;
     }
     if (edge_exists_already) {
       console.log("Edge cannot be added, it already exists");
     }
-    let edge = new Edge(node1, node2);
+    let edge = new Edge(vertex1, vertex2);
     this.edges.add(edge);
   }
 
   remove_edge(edge) {
     if (this.edges.has(edge)) {
       this.edges.delete(edge);
-      edge.node1.edges.delete(edge);
-      edge.node2.edges.delete(edge);
+      edge.vertex1.edges.delete(edge);
+      edge.vertex2.edges.delete(edge);
     } else {
       console.log("Edge not present, can't be removed!");
     }
   }
 
+  has_path(vertex1, vertex2) {
+    let reachable_vertexs = new Set();
+    let unexplored_vertexs = new Set([vertex1]);
+    let explored_vertexs = new Set();
+    console.log("Checking paths");
+    while (unexplored_vertexs.size > 0) {
+      let uv_list = Array.from(unexplored_vertexs);
+      for (let vertex of uv_list) {
+        unexplored_vertexs.delete(vertex);
+        explored_vertexs.add(vertex)
+        for (let edge of vertex.edges) {
+          let nb_vertex = edge.other_vertex(vertex);
+          reachable_vertexs.add(nb_vertex);
+          unexplored_vertexs.delete(nb_vertex);
+          if (!explored_vertexs.has(nb_vertex)) {
+            unexplored_vertexs.add(nb_vertex);
+          }
+
+        }
+      }
+    }
+    return reachable_vertexs.has(vertex2);
+  }
+
+  clear() {
+    this.vertexs.clear();
+    this.edges.clear();
+  }
+
   display() {
-    for (let node of this.nodes) {
-      node.display();
+    for (let vertex of this.vertexs) {
+      vertex.display();
     }
     for (let edge of this.edges) {
       edge.display();
@@ -219,9 +275,9 @@ class Ant {
     this.scene = scene;
     this.is_back_tracing = false;
     this.back_trace_list = [];
-    this.from_node = null;
+    this.from_vertex = null;
     this.edge = null;
-    this.to_node = scene.nest_node;
+    this.to_vertex = scene.nest_vertex;
     this.progress_on_edge = 0;
     this.has_food = false;
     this.radius = 12
@@ -252,19 +308,19 @@ class Ant {
       if (this.at_food() && !this.has_food) {
         this.has_food = true;
         this.is_back_tracing = true;
-        this.back_trace_list.push(this.from_node);
+        this.back_trace_list.push(this.from_vertex);
       } else if (this.at_nest() && this.has_food) {
         this.has_food = false;
         console.log("Dropping off food")
         this.is_back_tracing = false;
-        this.back_trace_list = [this.scene.nest_node];
+        this.back_trace_list = [this.scene.nest_vertex];
       }
       this.pick_new_edge();
     }
   }
 
   get_position() {
-    return this.edge.get_position(this.from_node, this.progress_on_edge);
+    return this.edge.get_position(this.from_vertex, this.progress_on_edge);
   }
 
   deposit_pheromone() {
@@ -273,26 +329,26 @@ class Ant {
   }
 
   pick_new_edge() {
-    let prev_node = this.from_node;
+    let prev_vertex = this.from_vertex;
     if (this.back_trace && !this.is_back_tracing) {
-      this.back_trace_list.push(prev_node);
+      this.back_trace_list.push(prev_vertex);
     }
-    this.from_node = this.to_node;
+    this.from_vertex = this.to_vertex;
     if (this.has_food && this.back_trace && this.is_back_tracing) {
-      this.to_node = this.back_trace_list[this.back_trace_list.length - 1];
-      let first_occurence = this.back_trace_list.indexOf(this.to_node);
+      this.to_vertex = this.back_trace_list[this.back_trace_list.length - 1];
+      let first_occurence = this.back_trace_list.indexOf(this.to_vertex);
       this.back_trace_list = this.back_trace_list.slice(0, first_occurence);
-      this.edge = this.to_node.find_edge(this.from_node);
-    } else{ 
+      this.edge = this.to_vertex.find_edge(this.from_vertex);
+    } else {
       let edges = []; // Not sure if I should stick to sets. Very low indexes anyway.
       let total_pheromone = 0;
-      for (let edge of this.from_node.edges) { // how do I force error checking?
+      for (let edge of this.from_vertex.edges) { // how do I force error checking?
         let add_edge = false;
-        if (!edge.has_node(prev_node)) {
+        if (!edge.has_vertex(prev_vertex)) {
           add_edge = true;
-        } else if (this.from_node.edges.size==1) {
+        } else if (this.from_vertex.edges.size == 1) {
           add_edge = true;
-        } else  if (this.at_nest() || this.at_food()) {
+        } else if (this.at_nest() || this.at_food()) {
           add_edge = true;
         }
         if (add_edge) {
@@ -300,26 +356,30 @@ class Ant {
           total_pheromone += edge.pheromone;
         }
       }
+      if (edges.length == 0) {
+        console.log("No walking options from vertex ", this.from_vertex);
+        console.log("Edges connected are ", this.from_vertex.edges);
+      }
       let it = -1;
       let pher_indicator = random(0, total_pheromone);
       do {
         it++;
         pher_indicator -= edges[it].pheromone;
       } while (pher_indicator > 0);
-      this.to_node = edges[it].other_node(this.from_node);
+      this.to_vertex = edges[it].other_vertex(this.from_vertex);
       this.edge = edges[it];
     }
     this.progress_on_edge = 0;
   }
 
   at_nest() {
-    // Assuming a node is reached
-    return this.to_node === this.scene.nest_node;
+    // Assuming a vertex is reached
+    return this.to_vertex === this.scene.nest_vertex;
   }
 
   at_food() {
-    // Assuming a node is reached
-    return this.scene.food_nodes.indexOf(this.to_node) >= 0;
+    // Assuming a vertex is reached
+    return this.scene.food_vertexs.indexOf(this.to_vertex) >= 0;
   }
 }
 
